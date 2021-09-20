f9d36a8ebcd433a23295d0b96c5321a9c69ffba5
Uri Zonens <urizonens@gmail.com>
2021-09-21 00:24:38 +0300

    add tests

--- test/testClient.js
+++ test/testClient.js
@@ -1,4 +1,17 @@
 const DAO = require ('../redisDao');
-let val =new DAO().getItem("key1");
-console.log(val);
+const expect    = require("chai").expect;
+
+describe("first test", function() {
+
+    it ("get item should be a function", ()=>{
+        expect(new DAO().getItem).to.be.a('function');
+    });
+
+    it("val should be val1", function() {
+
+        let val =new DAO().getItem("key1");
+        expect(val).to.equal("val1");
+
+    });
+  });
 
