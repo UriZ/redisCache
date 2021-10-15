0f6bb612ba62aeefdb2a11f332c1cb80e3b619b0
Uri Zonens <urizonens@gmail.com>
2021-10-16 00:56:07 +0300

    add retry on error

--- src/redisClient.js
+++ src/redisClient.js
@@ -9,11 +9,12 @@ promisifyAll(redis);
  * 
  * @returns a connected  client, after promisfication
  */
-let init = async ()=>{
+let init = async (retry = 0)=>{
 
     // 'urirediscluster.jsn1zq.clustercfg.use1.cache.amazonaws.com:6379'
     // const client = redis.createClient('urirediscluster.jsn1zq.clustercfg.use1.cache.amazonaws.com:6379');
 
+    
 
     const client = redis.createClient({
         host: 'redis-13765.c250.eu-central-1-1.ec2.cloud.redislabs.com',
@@ -22,6 +23,10 @@ let init = async ()=>{
 
     client.on('error', err => {
         console.log('Error in redis ' + err);
+        if (retry == 0)
+            return init(retry+1);
+        else
+            throw new Error("on error event called multiple times");
     });
 
     return client;
